b0383fb80135586891d76e869e466fc899287a5f
Sammarques88 <sampah21.sp@gmail.com>
2025-02-06 11:39:26 -0300

    While e do while

---
+++ ex012/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $i = 1;
+
+while ($i <=5){
+    echo $i . "<br>"; //imprime o valor de $i
+    $i++; //i+1
+}
+
+echo "<br>";
+//Mostrar somente números pares
+$f = 2;
+
+while ($f <= 10){
+    echo $f . "<br>";
+    $f += 2; //Incrementa de 2 em 2
+}
+echo "<br>";
+//Do While
+$g = 1;
+
+do{
+    echo $g . "<br>";
+    $g++;
+} while ($g <=7);
+?>
+
+    
+</body>
+</html>
+    
+</body>
+</html>
